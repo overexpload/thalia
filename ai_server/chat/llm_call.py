b4badb67f9c3faff543e2b3644e8ba81f6892acf
Izam Mohammed <izamdeveloper1@gmail.com>
2024-03-08 03:04:28 +0530

    feat: completed the initial setup in langchain

---
+++ ai_server/chat/llm_call.py
@@ -0,0 +1,29 @@
+from typing import Dict, Union
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+def call(input: str) -> Dict[str, Union[bool, str]]:
+    if "GOOGLE_API_KEY" not in os.environ:
+        return {
+            "success": False,
+            "message": "Gemini API not found",
+            }
+        
+    prompt = ChatPromptTemplate.from_template("be like an assistant and answer to this:  {prompt}")
+    model = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
+    chain  = prompt | model | StrOutputParser()
+    
+    response:str = chain.invoke({"prompt": input})
+    
+    if not response:
+        return {
+            "success": False,
+            "message": "Error in generation",
+            }
+        
+    return {
+        "success": True,
+        "message": response,
+        }
--- ai_server/chat/views.py
+++ ai_server/chat/views.py
@@ -1,7 +1,8 @@
+from typing import Dict, Union
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
+from .llm_call import call as llm_call
 
 @api_view(["POST"])
 def index(request):
@@ -17,8 +18,22 @@ def index(request):
             {"success": False, "message": "query should be str !"},
             status=status.HTTP_400_BAD_REQUEST,
         )
+    if not query:
+        return Response(
+            {"success": False, "message": "query should not be empty"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
         
+    response:Dict[str, Union[bool, str]] = llm_call(query)
+    
+    if not response["success"]:
+        return Response(
+            response,
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+    
     return Response(
-        {"success": True, "message": f"answer - {query}"},
+        response,
         status=status.HTTP_200_OK,
     )
+  
