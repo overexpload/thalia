d5f97c39ddc5be61507b4a812686a36ea1145700
Sreesanjay <sreesanjay7592@gmail.com>
2024-03-09 00:10:47 +0530

    feat: added join functionality

--- server/routes/communityRoute.js
+++ server/routes/communityRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { isLogedIn } = require('../middlewares/authMiddleware');
 const router = express.Router();
+const { createCommunity, getSuggestions,joinCommunity } = require('../controller/community')
 
-// router.get('/get-suggestions', isLogedIn, getSuggestions)
+router.get('/get-suggestions', isLogedIn, getSuggestions)
 router.post('/', isLogedIn, createCommunity)
+router.post('/join', isLogedIn, joinCommunity)
 module.exports = router;
