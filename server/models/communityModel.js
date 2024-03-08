2e4f4e420287680bf82852ebce191a021d2b3e1e
Sreesanjay <sreesanjay7592@gmail.com>
2024-03-08 21:40:13 +0530

    feat: community schema crated

---
+++ server/models/communityModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const communitySchema = new mongoose.Schema({
+    community_name: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+    },
+    about: {
+        type: String,
+    },
+    privacy: {
+        type: 'string',
+        enum: ['private', 'public']
+    },
+    is_delete: {
+        type: Boolean,
+        default: false
+    },
+
+});
+
+
+module.exports = mongoose.model("Community", communitySchema);
