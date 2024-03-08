612c545d7deb88656f349c989dbd7f646933070d
Sreesanjay <sreesanjay7592@gmail.com>
2024-03-08 11:41:57 +0530

    fix: removed deprecated dependancies

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 //db connection
 const connect = () => {
-    mongoose.connect(process.env.DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(() => console.log('Connected to db')).catch((e) => {
+    mongoose.connect(process.env.DB_URL).then(() => console.log('Connected to db')).catch((e) => {
         console.error(e.message);
     })
 }
