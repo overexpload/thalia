75ea2d6beed39e169c2126d90e9f0e2c4a3bf019
Shuhaib T U <shuhaibtu79@gmail.com>
2024-03-08 19:52:20 +0530

    style: navebar bg-color changed

--- client/src/components/Nabar/Navbar.jsx
+++ client/src/components/Nabar/Navbar.jsx
@@ -4,12 +4,12 @@ import Logo from "../../assets/Logo.svg";
 export default function Navbar() {
   return (
     <>
-      <div className="flex sticky z-20 top-0 w-full px-10 py-1 bg-background">
+      <div className="flex sticky z-40 top-0 w-full px-10 py-1 bg-gradient-to-r from-[#2d1525] to-background">
         <Link to={"/"}>
           <img src={Logo} className="w-24" alt="" />
         </Link>
       </div>
-      <div className="w-full h-[0.1px] bg-gray-700"></div>
+      <div className="w-full h-[0.1px] z-40 bg-gray-800"></div>
     </>
   );
 }
