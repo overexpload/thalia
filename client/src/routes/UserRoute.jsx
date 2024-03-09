011593b4d093d9084587951ba3890f09d0116db0
Shuhaib T U <shuhaibtu79@gmail.com>
2024-03-09 21:36:03 +0530

    feat: My laws, my Body, my Mind pages added

--- client/src/routes/UserRoute.jsx
+++ client/src/routes/UserRoute.jsx
@@ -7,6 +7,15 @@ import Protect from "../components/Auth/Protect";
 const Home = lazy(() => import("../pages/UserPages/Home/Home"));
 const Signup = lazy(() => import("../pages/UserPages/Signup/Signup"));
 const Login = lazy(() => import("../pages/UserPages/Login/Login"));
+const MyMindPage = lazy(() =>
+  import("../pages/UserPages/MyMindPage/MyMindPage")
+);
+const MyLawsPage = lazy(() =>
+  import("../pages/UserPages/MyLawsPage/MyLawsPage")
+);
+const MyBodyPage = lazy(() =>
+  import("../pages/UserPages/MyBodyPage/MyBodyPage")
+);
 
 export default function UserRoute() {
   return (
@@ -20,6 +29,9 @@ export default function UserRoute() {
           </Route>
           <Route element={<Protect role="USER" />}>
             <Route path={"/home"} element={<Home />} />
+            <Route path={"/my-mind"} element={<MyMindPage />} />
+            <Route path={"/my-rights"} element={<MyLawsPage />} />
+            <Route path={"/my-body"} element={<MyBodyPage />} />
           </Route>
         </Routes>
       </Suspense>
