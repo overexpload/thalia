633a645c928f0407fa5d1d9401410e19799b7ffe
Shuhaib T U <shuhaibtu79@gmail.com>
2024-03-10 03:18:18 +0530

    feat: thaliaAI-chatbot ui completed

---
+++ client/src/components/Loader/Loader3/Loader3.jsx
@@ -0,0 +1,11 @@
+import "./Loader3.css";
+
+export default function Loader3() {
+  return (
+    <div className="load">
+      <div className="progress"></div>
+      <div className="progress"></div>
+      <div className="progress"></div>
+    </div>
+  );
+}
