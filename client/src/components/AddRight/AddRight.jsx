8d3095057b825635eba15606ad8e56b87cf7aafa
Amarnath AS <amarnathas2003@gmail.com>
2024-03-10 02:49:14 +0530

    feat: admin add right page update

--- client/src/components/AddRight/AddRight.jsx
+++ client/src/components/AddRight/AddRight.jsx
@@ -68,7 +68,7 @@ function AddRight({ setOpenModal, openModal }) {
             </label>
           </div>
           <button
-            className="text-primary border-2 px-2 py-2 rounded-md float-end"
+            className="text-primary border-2 px-5 py-1 hover:bg-primary hover:text-black rounded-md float-end"
             onClick={handleCreate}
           >
             Add Right
