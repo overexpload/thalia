447631ead86b12da351e35c2e280044823223f2a
Shuhaib T U <shuhaibtu79@gmail.com>
2024-03-09 10:54:36 +0530

    fix: updated the thaliaAPI

--- client/src/API/thaliaAPI.js
+++ client/src/API/thaliaAPI.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
-
 const baseURL = import.meta.env.VITE_BASE_URL;
 const thaliaAPI = axios.create({
-    baseURL
+    baseURL,
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
 
 export default thaliaAPI;
